d9b8e3e1c2befdbefbb5229087ed783136dd9393
Vítor Schuh <vitorschuh@MacBook-Air-de-Vitor.local>
2022-07-18 01:26:42 -0300

    fix: fixing exist_ok flag to True

--- pull.py
+++ pull.py
@@ -2,9 +2,9 @@ import os
 
 
 def create_airflow_folders() -> None:
-    os.makedirs(name="./dags", exist_ok=False)
-    os.makedirs(name="./logs", exist_ok=False)
-    os.makedirs(name="./plugins", exist_ok=False)
+    os.makedirs(name="./dags", exist_ok=True)
+    os.makedirs(name="./logs", exist_ok=True)
+    os.makedirs(name="./plugins", exist_ok=True)
 
 if __name__ == "__main__":
     create_airflow_folders()
